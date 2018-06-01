b0270548f3fe65a863d86821834af447dcaa2e0c
Guillaume Baudart <guillaume.baudart@ibm.com>
2018-06-01 13:55:24 -0400

    Reorganize following stan.g4

--- deepppl/translation/stan2python.py
+++ deepppl/translation/stan2python.py
@@ -42,61 +42,39 @@ class Printer(stanListener):
     def __init__(self):
         self.indentation = 0
 
-    def exitProgram(self, ctx):
-        ctx.ast = Module()
-        ctx.ast.body = [
-            Import(names=[alias(name='torch', asname=None)]),
-            ImportFrom(
-                module='torch.distributions',
-                names=[alias(name='*', asname=None)],
-                level=0)]
-        for child in ctx.children:
-            if hasattr(child, 'ast'):
-                ctx.ast.body += child.ast
-        ast.fix_missing_locations(ctx.ast)
-        # print(ast.dump(ctx.ast))
-        print(astor.to_source(ctx.ast))
+    def exitVariableDecl(self, ctx):
+        vid = ctx.IDENTIFIER().getText()
+        if ctx.arrayDim() is not None:
+            dims = parseExpr(ctx.arrayDim().getText())
+            ctx.ast = [Assign(
+                targets=[Name(id=vid, ctx=Store())],
+                value=Call(func=Attribute(
+                    value=Name(id='torch', ctx=Load()),
+                    attr='zeros', ctx=Load()),
+                    args=[dims],
+                    keywords=[]))]
+        else:
+            ctx.ast = [Assign(
+                targets=[Name(id=vid, ctx=Store())],
+                value=Call(func=Attribute(
+                    value=Name(id='torch', ctx=Load()),
+                    attr='zeros', ctx=Load()),
+                    args=[List(elts=[], ctx=Load())],
+                    keywords=[]))]
 
-    def exitDataBlock(self, ctx):
+    def exitVariableDeclsOpt(self, ctx):
         ctx.ast = gatherChildrenAST(ctx)
 
-    def exitParametersBlock(self, ctx):
-        ctx.ast = gatherChildrenAST(ctx)
-
-    def exitModelBlock(self, ctx):
-        ctx.ast = gatherChildrenAST(ctx)
-
-    def exitExpressionCommaList(self, ctx):
-        ctx.ast = gatherChildrenAST(ctx)
+    # Vector, matrix and array expressions (section 4.2)
 
     def exitExpression(self, ctx):
         ctx.ast = [parseExpr(ctx.getText())]
 
-    def exitStatementsOpt(self, ctx):
+    def exitExpressionCommaList(self, ctx):
         ctx.ast = gatherChildrenAST(ctx)
 
-    def exitStatement(self, ctx):
-        if ctx.forStmt() is not None:
-            ctx.ast = [ctx.forStmt().ast]
-        if ctx.samplingStmt() is not None:
-            ctx.ast = [ctx.samplingStmt().ast]
-
-    def exitForStmt(self, ctx):
-        id = ctx.IDENTIFIER().getText()
-        body = []
-        if hasattr(ctx.statement(), 'ast'):
-            body = ctx.statement().ast
-        if len(ctx.atom()) > 1:
-            lbound = parseExpr(ctx.atom()[0].getText())
-            ubound = parseExpr(ctx.atom()[1].getText())
-            ctx.ast = For(
-                target=Name(id=id, ctx=Store()),
-                iter=Call(func=Name(
-                    id='range', ctx=Load()),
-                    args=[lbound, ubound],
-                    keywords=[]),
-                body=body,
-                orelse=[])
+    # Statements (section 5)
+    # Sampling (section 5.3)
 
     def exitSamplingStmt(self, ctx):
         lvalue = parseExpr(ctx.lvalueSampling().getText())
@@ -117,25 +95,72 @@ class Printer(stanListener):
                     args=[],
                     keywords=[]))
 
-    def exitVariableDeclsOpt(self, ctx):
+    # For loops (section 5.4)
+
+    def exitForStmt(self, ctx):
+        id = ctx.IDENTIFIER().getText()
+        body = []
+        if hasattr(ctx.statement(), 'ast'):
+            body = ctx.statement().ast
+        if len(ctx.atom()) > 1:
+            lbound = parseExpr(ctx.atom()[0].getText())
+            ubound = parseExpr(ctx.atom()[1].getText())
+            ctx.ast = For(
+                target=Name(id=id, ctx=Store()),
+                iter=Call(func=Name(
+                    id='range', ctx=Load()),
+                    args=[lbound, ubound],
+                    keywords=[]),
+                body=body,
+                orelse=[])
+
+    # statements
+
+    def exitStatement(self, ctx):
+        if ctx.assignStmt() is not None:
+            assert False, "Not yet implemented"
+        if ctx.samplingStmt() is not None:
+            ctx.ast = [ctx.samplingStmt().ast]
+        if ctx.forStmt() is not None:
+            ctx.ast = [ctx.forStmt().ast]
+        if ctx.conditionalStmt() is not None:
+            assert False, "Not yet implemented"
+        if ctx.whileStmt() is not None:
+            assert False, "Not yet implemented"
+        if ctx.blockStmt() is not None:
+            assert False, "Not yet implemented"
+        if ctx.callStmt() is not None:
+            assert False, "Not yet implemented"
+        if ctx.BREAK() is not None:
+            assert False, "Not yet implemented"
+        if ctx.CONTINUE() is not None:
+            assert False, "Not yet implemented"
+
+    def exitStatementsOpt(self, ctx):
         ctx.ast = gatherChildrenAST(ctx)
 
-    def exitVariableDecl(self, ctx):
-        vid = ctx.IDENTIFIER().getText()
-        if ctx.arrayDim() is not None:
-            dims = parseExpr(ctx.arrayDim().getText())
-            ctx.ast = [Assign(
-                targets=[Name(id=vid, ctx=Store())],
-                value=Call(func=Attribute(
-                    value=Name(id='torch', ctx=Load()),
-                    attr='zeros', ctx=Load()),
-                    args=[dims],
-                    keywords=[]))]
-        else:
-            ctx.ast = [Assign(
-                targets=[Name(id=vid, ctx=Store())],
-                value=Call(func=Attribute(
-                    value=Name(id='torch', ctx=Load()),
-                    attr='zeros', ctx=Load()),
-                    args=[List(elts=[], ctx=Load())],
-                    keywords=[]))]
+    # Program blocks (section 6)
+
+    def exitDataBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
+
+    def exitParametersBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
+
+    def exitModelBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
+
+    def exitProgram(self, ctx):
+        ctx.ast = Module()
+        ctx.ast.body = [
+            Import(names=[alias(name='torch', asname=None)]),
+            ImportFrom(
+                module='torch.distributions',
+                names=[alias(name='*', asname=None)],
+                level=0)]
+        for child in ctx.children:
+            if hasattr(child, 'ast'):
+                ctx.ast.body += child.ast
+        ast.fix_missing_locations(ctx.ast)
+        astpretty.pprint(ctx.ast)
+        print(astor.to_source(ctx.ast))
