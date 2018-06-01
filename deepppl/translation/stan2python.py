9946f006d4215a81d180a8dd7b9e5ac93cd8d6cb
Guillaume Baudart <guillaume.baudart@ibm.com>
2018-06-01 13:38:03 -0400

    Generate python AST in stan2python

--- deepppl/translation/stan2python.py
+++ deepppl/translation/stan2python.py
@@ -15,61 +15,127 @@
 '''
 
 import sys
+import ast
+from ast import *
 from antlr4 import *
 from parser.stanParser import stanParser
 from parser.stanListener import stanListener
+import astor
+import astpretty
+
+
+def parseExpr(expr):
+    node = ast.parse(expr).body[0].value
+    return node
+
+
+def gatherChildrenAST(ctx):
+    ast = []
+    if ctx.children is not None:
+        for child in ctx.children:
+            if hasattr(child, 'ast') and child.ast is not None:
+                ast += child.ast
+        return ast
 
 
 class Printer(stanListener):
     def __init__(self):
         self.indentation = 0
 
-    def dump(self, s):
-        print(' ' * self.indentation * 4 + s)
+    def exitProgram(self, ctx):
+        ctx.ast = Module()
+        ctx.ast.body = [
+            Import(names=[alias(name='torch', asname=None)]),
+            ImportFrom(
+                module='torch.distributions',
+                names=[alias(name='*', asname=None)],
+                level=0)]
+        for child in ctx.children:
+            if hasattr(child, 'ast'):
+                ctx.ast.body += child.ast
+        ast.fix_missing_locations(ctx.ast)
+        # print(ast.dump(ctx.ast))
+        print(astor.to_source(ctx.ast))
 
-    def enterProgram(self, ctx):
-        self.dump('import torch')
-        self.dump('from torch.distributions import *')
+    def exitDataBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
 
-    def enterDataBlock(self, ctx):
-        self.dump('\n# Data')
+    def exitParametersBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
 
-    def enterParametersBlock(self, ctx):
-        self.dump('\n# Parameters')
+    def exitModelBlock(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
 
-    def enterModelBlock(self, ctx):
-        self.dump('\n# Model')
+    def exitExpressionCommaList(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
 
-    def enterForStmt(self, ctx):
-        id = ctx.IDENTIFIER().getText()
-        if len(ctx.atom()) > 1:
-            lbound = ctx.atom()[0].getText()
-            ubound = ctx.atom()[1].getText()
-            self.dump('for ' + id + ' in range(' +
-                      lbound + ',' + ubound + '):')
-        else:
-            it = ctx.atom()[0].getText()
-            self.dump('for ' + id + ' in ' + it + ':')
-        self.indentation += 1
+    def exitExpression(self, ctx):
+        ctx.ast = [parseExpr(ctx.getText())]
+
+    def exitStatementsOpt(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
+
+    def exitStatement(self, ctx):
+        if ctx.forStmt() is not None:
+            ctx.ast = [ctx.forStmt().ast]
+        if ctx.samplingStmt() is not None:
+            ctx.ast = [ctx.samplingStmt().ast]
 
     def exitForStmt(self, ctx):
-        self.indentation -= 1
+        id = ctx.IDENTIFIER().getText()
+        body = []
+        if hasattr(ctx.statement(), 'ast'):
+            body = ctx.statement().ast
+        if len(ctx.atom()) > 1:
+            lbound = parseExpr(ctx.atom()[0].getText())
+            ubound = parseExpr(ctx.atom()[1].getText())
+            ctx.ast = For(
+                target=Name(id=id, ctx=Store()),
+                iter=Call(func=Name(
+                    id='range', ctx=Load()),
+                    args=[lbound, ubound],
+                    keywords=[]),
+                body=body,
+                orelse=[])
 
-    def enterSamplingStmt(self, ctx):
-        lvalue = ctx.lvalueSampling().getText()
+    def exitSamplingStmt(self, ctx):
+        lvalue = parseExpr(ctx.lvalueSampling().getText())
         if ctx.PLUS_EQ() is not None:
             assert False, 'Not yet implemented'
         else:
-            assert len(ctx.IDENTIFIER()) == 1
-            id = ctx.IDENTIFIER()[0].getText()
-            expr = ctx.expressionCommaList().getText()
-            self.dump(lvalue + ' = ' + id.capitalize() +
-                      '(' + expr + ').sample()')
+            id = ctx.IDENTIFIER()[0].getText().capitalize()
+            exprList = ctx.expressionCommaList().ast
+            ctx.ast = Assign(
+                targets=[lvalue],
+                value=Call(func=Attribute(
+                    value=Call(func=Name(
+                        id=id, ctx=Load()),
+                        args=exprList,
+                        keywords=[]),
+                    attr='sample',
+                    ctx=Load()),
+                    args=[],
+                    keywords=[]))
 
-    def enterVariableDecl(self, ctx):
+    def exitVariableDeclsOpt(self, ctx):
+        ctx.ast = gatherChildrenAST(ctx)
+
+    def exitVariableDecl(self, ctx):
         vid = ctx.IDENTIFIER().getText()
         if ctx.arrayDim() is not None:
-            dims = ctx.arrayDim().getText()
-            self.dump(vid + ' = torch.zeros(' + dims + ')')
+            dims = parseExpr(ctx.arrayDim().getText())
+            ctx.ast = [Assign(
+                targets=[Name(id=vid, ctx=Store())],
+                value=Call(func=Attribute(
+                    value=Name(id='torch', ctx=Load()),
+                    attr='zeros', ctx=Load()),
+                    args=[dims],
+                    keywords=[]))]
         else:
-            self.dump(vid + ' = torch.zeros([])')
+            ctx.ast = [Assign(
+                targets=[Name(id=vid, ctx=Store())],
+                value=Call(func=Attribute(
+                    value=Name(id='torch', ctx=Load()),
+                    attr='zeros', ctx=Load()),
+                    args=[List(elts=[], ctx=Load())],
+                    keywords=[]))]
