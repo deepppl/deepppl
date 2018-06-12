ca2131ff48900886e0f171e6f5e1ab1e6761344a
jburroni <jburroni@users.noreply.github.com>
2018-06-12 14:54:32 -0400

    Basic test (#2)
    * Basic test infrastructure
    
    * use python files for the target

---
+++ deepppl/tests/test_irl.py
@@ -0,0 +1,61 @@
+# /*
+#  * Copyright 2018 IBM Corporation
+#  *
+#  * Licensed under the Apache License, Version 2.0 (the "License");
+#  * you may not use this file except in compliance with the License.
+#  * You may obtain a copy of the License at
+#  *
+#  * http://www.apache.org/licenses/LICENSE-2.0
+#  *
+#  * Unless required by applicable law or agreed to in writing, software
+#  * distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+# */
+
+import dpplc
+import ast
+import pytest
+
+def code_to_normalized(code):
+    return ast.dump(ast.parse(code))
+
+
+def test_coin():
+    filename = r'tests/good/coin.stan'
+    target_file = r'tests/target_py/coin.py'
+    with open(target_file) as f:
+        target_code = f.read() 
+    target = code_to_normalized(target_code)
+    
+    compiled = dpplc.stan2astpyFile(filename)
+    assert code_to_normalized(compiled) == target
+
+def test_coin_vectorized():
+    filename = r'tests/good/coin_vectorized.stan'
+    target_file = r'tests/target_py/coin_vectorized.py'
+    with open(target_file) as f:
+        target_code = f.read() 
+    target = code_to_normalized(target_code)
+    
+    compiled = dpplc.stan2astpyFile(filename)
+    assert code_to_normalized(compiled) == target
+
+
+
+@pytest.mark.xfail
+def test_coin_reverted_lines():
+    """Inside a `block`, stan semantics do not requires lines to be 
+    ordered.
+    """
+    filename = r'tests/good/coin_reverted.stan'
+    target_file = r'tests/target_py/coin_vectorized.py'
+    with open(target_file) as f:
+        target_code = f.read() 
+    target = code_to_normalized(target_code)
+    
+    compiled = dpplc.stan2astpyFile(filename)
+    assert code_to_normalized(compiled) == target
+
+
