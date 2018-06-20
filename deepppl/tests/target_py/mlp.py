33c39e52b75e124d9d04d3df60efe421f3f1add9
Javier Burroni <javier.burroni@ibm.com>
2018-06-20 09:42:11 -0400

    use `exp` for scale

--- deepppl/tests/target_py/mlp.py
+++ deepppl/tests/target_py/mlp.py
@@ -6,20 +6,19 @@ import pyro.distributions as dist
 def guide_mlp(batch_size, imgs, labels):
     guide_mlp = {}
     l1wloc = pyro.param('l1wloc', randn(0, 1))
-    l1wscale = pyro.param('l1wscale', randn(0, 1))
+    l1wscale = pyro.param('l1wscale', exp(randn()))
     guide_mlp['l1.weight'] = dist.Normal(l1wloc, l1wscale)
     l1bloc = pyro.param('l1bloc', randn(0, 1))
-    l1bscale = pyro.param('l1bscale', randn(0, 1))
+    l1bscale = pyro.param('l1bscale', exp(randn()))
     guide_mlp['l1.bias'] = dist.Normal(l1bloc, l1bscale)
     l2wloc = pyro.param('l2wloc', randn(0, 1))
-    l2wscale = pyro.param('l2wscale', randn(0, 1))
+    l2wscale = pyro.param('l2wscale', exp(randn()))
     guide_mlp['l2.weight'] = dist.Normal(l2wloc, l2wscale)
-    l2bloc = pyro.param('l2bloc', randn(0, 1))
-    l2bscale = pyro.param('l2bscale', randn(0, 1))
+    l2bloc = pyro.param('l2bloc', randn())
+    l2bscale = pyro.param('l2bscale', exp(randn()))
     guide_mlp['l2.bias'] = dist.Normal(l2bloc, l2bscale)
     lifted_mlp = pyro.random_module('mlp', mlp, guide_mlp)
     return lifted_mlp()
-
 
 def prior_mlp():
     prior_mlp = {}
