a7fa644a0e7620b127d51031ea40c1a8a29e7664
Javier Burroni <javier.burroni@ibm.com>
2018-06-25 16:49:46 -0400

    uniform requires `FloatTensor`

--- deepppl/tests/target_py/coin_reverted.py
+++ deepppl/tests/target_py/coin_reverted.py
@@ -5,5 +5,5 @@ import pyro.distributions as dist
 
 
 def model(x):
-    theta = pyro.sample('theta', dist.Uniform(torch(0), torch(1)))
+    theta = pyro.sample('theta', dist.Uniform(torch(0.0), torch(1.0)))
     pyro.sample('x', dist.Bernoulli(theta), obs=x)
