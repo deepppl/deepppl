d9e22505e1bdd873f610b758c82761d7c97f799d
Javier Burroni <javier.burroni@ibm.com>
2018-06-27 13:47:32 -0400

    first setup.py version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='DeepPPL',
+      version='0.1',
+      description='Deep Probabilistic Programming Language',
+      url='https://github.com/deepppl/deepppl',
+      author='Many Authors',
+      author_email='many.authors@ibm.com',
+      license='Apache License 2.0',
+      packages=['deepppl', 'deepppl.parser', 'deepppl.translation'])
