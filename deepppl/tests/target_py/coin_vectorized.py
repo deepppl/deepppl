10ff854d3973018a654be787d1b152d90247a55b
Javier Burroni <javier.burroni@ibm.com>
2018-07-13 13:12:36 -0400

    add shape description to coin related examples #17

--- deepppl/tests/target_py/coin_vectorized.py
+++ deepppl/tests/target_py/coin_vectorized.py
@@ -5,5 +5,7 @@ import pyro.distributions as dist
 
 
 def model(x):
+    ___shape = {}
+    ___shape['x'] = tensor(10)
     theta = pyro.sample('theta', dist.Uniform(tensor(0.0), tensor(1.0)))
     pyro.sample('x', dist.Bernoulli(theta), obs=x)
