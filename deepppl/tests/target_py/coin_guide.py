4ab1b3a5915b645584520f7e23c5a1ddc8205d6d
Javier Burroni <javier.burroni@ibm.com>
2018-07-13 13:12:36 -0400

    add shape description to coin related examples #17

--- deepppl/tests/target_py/coin_guide.py
+++ deepppl/tests/target_py/coin_guide.py
@@ -5,13 +5,16 @@ import pyro.distributions as dist
 
 
 def guide_(x):
+    ___shape = {}
+    ___shape['x'] = tensor(10)
     alpha_q = pyro.param('alpha_q', tensor(15.0))
     beta_q = pyro.param('beta_q', tensor(15.0))
     theta = pyro.sample('theta', dist.Beta(alpha_q, beta_q))
 
 
 def model(x):
+    ___shape = {}
+    ___shape['x'] = tensor(10)
     theta = pyro.sample('theta', dist.Beta(tensor(10.0), tensor(10.0)))
     for i in range(tensor(1), tensor(10) + 1):
-        pyro.sample('x' + '{}'.format(i - 1), dist.Bernoulli(theta), obs=x[
-            i - 1])
+        pyro.sample('x' + '{}'.format(i - 1), dist.Bernoulli(theta), obs=x[i-1])
--- deepppl/tests/target_py/operators-expr.py
+++ deepppl/tests/target_py/operators-expr.py
@@ -5,6 +5,8 @@ import pyro.distributions as dist
 
 
 def model(x):
+    ___shape = {}
+    ___shape['x'] = tensor(10)
     theta = pyro.sample('theta', dist.Uniform(tensor(0) * tensor(3) / tensor(5), tensor(1) + tensor(5) - tensor(5)))
     for i in range(tensor(1), tensor(10) + 1):
         if tensor(1) <= tensor(10) and (tensor(1) > tensor(5) or tensor(2) < tensor(1)):
