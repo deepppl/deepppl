8f5295bb31f241c2c227073dc180b34ba024f59a
Javier Burroni <javier.burroni@ibm.com>
2018-07-16 11:21:02 -0400

    Shape of neural network's parameters is a property, not a function.

--- deepppl/tests/target_py/mlp.py
+++ deepppl/tests/target_py/mlp.py
@@ -8,14 +8,14 @@ def guide_mlp(batch_size, imgs, labels):
     ___shape = {}
     ___shape['imgs'] = [tensor(28), tensor(28), batch_size]
     ___shape['labels'] = batch_size
-    ___shape['l1wloc'] = mlp.l1.weight.shape()
-    ___shape['l1wscale'] = mlp.l1.weight.shape()
-    ___shape['l1bloc'] = mlp.l1.bias.shape()
-    ___shape['l1bscale'] = mlp.l1.bias.shape()
-    ___shape['l2wloc'] = mlp.l2.weight.shape()
-    ___shape['l2wscale'] = mlp.l2.weight.shape()
-    ___shape['l2bloc'] = mlp.l2.bias.shape()
-    ___shape['l2bscale'] = mlp.l2.bias.shape()
+    ___shape['l1wloc'] = mlp.l1.weight.shape
+    ___shape['l1wscale'] = mlp.l1.weight.shape
+    ___shape['l1bloc'] = mlp.l1.bias.shape
+    ___shape['l1bscale'] = mlp.l1.bias.shape
+    ___shape['l2wloc'] = mlp.l2.weight.shape
+    ___shape['l2wscale'] = mlp.l2.weight.shape
+    ___shape['l2bloc'] = mlp.l2.bias.shape
+    ___shape['l2bscale'] = mlp.l2.bias.shape
     guide_mlp = {}
     l1wloc = pyro.param('l1wloc', randn(___shape['l1wloc']))
     l1wscale = pyro.param('l1wscale', exp(randn(___shape['l1wscale'])))
@@ -37,14 +37,14 @@ def prior_mlp(batch_size, imgs, labels):
     ___shape['imgs'] = [tensor(28), tensor(28), batch_size]
     ___shape['labels'] = batch_size
     prior_mlp = {}
-    prior_mlp['l1.weight'] = dist.Normal(zeros(mlp.l1.weight.shape()), ones
-        (mlp.l1.weight.shape()))
-    prior_mlp['l1.bias'] = dist.Normal(zeros(mlp.l1.bias.shape()), ones(mlp
-        .l1.bias.shape()))
-    prior_mlp['l2.weight'] = dist.Normal(zeros(mlp.l2.weight.shape()), ones
-        (mlp.l2.weight.shape()))
-    prior_mlp['l2.bias'] = dist.Normal(zeros(mlp.l2.bias.shape()), ones(mlp
-        .l2.bias.shape()))
+    prior_mlp['l1.weight'] = dist.Normal(zeros(mlp.l1.weight.shape), ones
+        (mlp.l1.weight.shape))
+    prior_mlp['l1.bias'] = dist.Normal(zeros(mlp.l1.bias.shape), ones(mlp
+        .l1.bias.shape))
+    prior_mlp['l2.weight'] = dist.Normal(zeros(mlp.l2.weight.shape), ones
+        (mlp.l2.weight.shape))
+    prior_mlp['l2.bias'] = dist.Normal(zeros(mlp.l2.bias.shape), ones(mlp
+        .l2.bias.shape))
     lifted_mlp = pyro.random_module('mlp', mlp, prior_mlp)
     return lifted_mlp()
 
