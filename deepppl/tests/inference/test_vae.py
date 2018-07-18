4f9e175dbf9f46803a00699aab8df1f60e5d043d
Javier Burroni <javier.burroni@ibm.com>
2018-07-18 18:59:10 -0400

    remove duplicated code #21

--- deepppl/tests/inference/test_vae.py
+++ deepppl/tests/inference/test_vae.py
@@ -6,11 +6,7 @@ import pyro
 from pyro import distributions as dist
 import numpy as np
 
-import torch.utils.data.dataloader as dataloader
-import torchvision
-from torchvision import transforms
-from torchvision.datasets import MNIST
-
+from util import loadData
 import deepppl
 import os
 
@@ -50,20 +46,6 @@ def build_vae():
     return Encoder(), Decoder()
 
 
-
-def loadData():
-    train = MNIST(os.environ.get("DATA_DIR", '.') + "/data", train=True, download=True, transform=transforms.Compose([
-        transforms.ToTensor(),  # ToTensor does min-max normalization.
-    ]), )
-    test = MNIST(os.environ.get("DATA_DIR", '.') + "/data", train=False, download=True, transform=transforms.Compose([
-        transforms.ToTensor(),  # ToTensor does min-max normalization.
-    ]), )
-    dataloader_args = dict(shuffle=True, batch_size=batch_size,
-                        num_workers=3, pin_memory=False)
-    train_loader = dataloader.DataLoader(train, **dataloader_args)
-    test_loader = dataloader.DataLoader(test, **dataloader_args)
-    return train_loader, test_loader
-
 def distance(x, y):
     return torch.norm(x - y, dim=1).data.numpy()
 
@@ -89,7 +71,7 @@ class Classifier:
 
 def test_vae_inference():
     encoder, decoder = build_vae()
-    train_loader, test_loader = loadData()
+    train_loader, test_loader = loadData(batch_size)
     model = deepppl.DppplModel(
                                 model_file = 'deepppl/tests/good/vae.stan', 
                                 encoder=encoder,
