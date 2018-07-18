e805a008ea845e5c9f068b60c9ad9125c04909d4
Javier Burroni <javier.burroni@ibm.com>
2018-07-18 18:40:37 -0400

    test code for inference in the `vae` example. This example illustrates how to use batch_size for sampling and observing. #22

--- deepppl/tests/target_py/vae.py
+++ deepppl/tests/target_py/vae.py
@@ -4,19 +4,19 @@ import pyro
 import pyro.distributions as dist
 
 
-def guide_(x):
+def guide_(batch_size, nz, x):
     ___shape = {}
     pyro.module('encoder', encoder)
     ___shape['encoded'] = tensor(2)
     encoded = encoder(x)
     mu = encoded[tensor(1) - 1]
     sigma = encoded[tensor(2) - 1]
-    latent = pyro.sample('latent', dist.Normal(mu, sigma))
+    latent = pyro.sample('latent', dist.Normal(mu, sigma, batch_size))
 
 
-def model(x):
+def model(batch_size, nz, x):
     ___shape = {}
-    pyro.module("decoder", decoder)
-    latent = pyro.sample('latent', dist.Normal(tensor(0), tensor(1)))
+    pyro.module('decoder', decoder)
+    latent = pyro.sample('latent', dist.Normal(zeros(nz), ones(nz), batch_size))
     loc_img = decoder(latent)
     pyro.sample('x', dist.Bernoulli(loc_img), obs=x)
