3b6f2f813709737ac4f316865942ddca4c08e60d
Javier Burroni <javier.burroni@ibm.com>
2018-08-21 12:00:33 -0400

    update tests to use new compilation scheme

--- deepppl/tests/target_py/coin_guide.py
+++ deepppl/tests/target_py/coin_guide.py
@@ -15,7 +15,9 @@ def guide_(x):
 def model(x):
     ___shape = {}
     ___shape['x'] = tensor(10)
-    theta = pyro.sample('theta', dist.Uniform(tensor(0), tensor(1)))
-    pyro.sample('theta', dist.Beta(tensor(10.0), tensor(10.0)), obs=theta)
+    theta = pyro.sample('theta', dist.Uniform(tensor(0.0), tensor(1.0)))
+    pyro.sample('theta' + '1', dist.Beta(tensor(10.0), tensor(10.0)), obs=theta
+        )
     for i in range(tensor(1), tensor(10) + 1):
-        pyro.sample('x' + '{}'.format(i - 1), dist.Bernoulli(theta), obs=x[i-1])
+        pyro.sample('x' + '{}'.format(i - 1) + '2', dist.Bernoulli(theta),
+            obs=x[i - 1])
--- deepppl/tests/target_py/coin_vectorized.py
+++ deepppl/tests/target_py/coin_vectorized.py
@@ -8,5 +8,5 @@ def model(x):
     ___shape = {}
     ___shape['x'] = tensor(10)
     theta = pyro.sample('theta', dist.Uniform(tensor(0.0), tensor(1.0)))
-    pyro.sample('theta', dist.Beta(tensor(10.0), tensor(10.0)), obs=theta)
-    pyro.sample('x', dist.Bernoulli(theta), obs=x)
+    pyro.sample('theta' + '1', dist.Uniform(tensor(0.0), tensor(1.0)), obs=theta)
+    pyro.sample('x' + '2', dist.Bernoulli(theta), obs=x)
