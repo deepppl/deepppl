116260eeca0bb88973979addda67d8e3e00c72bd
Javier Burroni <javier.burroni@ibm.com>
2018-08-22 11:54:30 -0400

    test inference on example using `theta ~ normal()` twice

---
+++ deepppl/tests/inference/test_double_gaussian.py
@@ -0,0 +1,31 @@
+import torch
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch import tensor
+import pyro
+from pyro import distributions as dist
+import numpy as np
+from pyro.infer import mcmc
+
+import deepppl
+import os
+import pandas as pd
+
+def nuts(model, **kwargs):
+    nuts_kernel = mcmc.NUTS(model, adapt_step_size = True)
+    return mcmc.MCMC(nuts_kernel, **kwargs)
+
+def test_gaussian_inference():
+    model = deepppl.DppplModel(model_file = 'deepppl/tests/good/double_gaussian.stan')
+
+    posterior = model.posterior(
+                method=nuts, 
+                num_samples=3000, 
+                warmup_steps=300)
+
+    marginal = pyro.infer.EmpiricalMarginal(posterior.run(), sites='theta')
+
+    series = pd.Series([marginal().item() for _ in range(3000)], name = r'$\theta$')
+    assert np.abs(series.mean() - 1000) < 1
+    assert np.abs(series.var() - .5) < 0.1 
