ca65f3f2af9ac39706e64773882d593699f52946
kakate@us.ibm.com <kakate@us.ibm.com>
2018-12-14 13:11:54 -0500

    Adding tests for linear regression

---
+++ deepppl/tests/inference/test_linear_regression.py
@@ -0,0 +1,47 @@
+
+import torch
+import pyro
+import pyro.distributions as dist
+from pyro.infer import mcmc
+import deepppl
+import os
+import numpy as np
+from utils.utils import ImproperUniform
+
+torch.manual_seed(11)
+
+# def model(N, x, y):
+#     alpha = pyro.sample('alpha', ImproperUniform())
+#     beta = pyro.sample('beta', ImproperUniform())
+#     sigma = pyro.sample('sigma', ImproperUniform())
+#     pyro.sample('y' + '1', dist.Normal(alpha + beta * x, sigma), obs=y)
+
+def model(N, x, y):
+    alpha = pyro.sample('alpha', dist.Normal(0, 1000))
+    beta = pyro.sample('beta', dist.Normal(0, 1000))
+    sigma = pyro.sample('sigma', dist.Uniform(0, 1000))
+    pyro.sample('y' + '1', dist.Normal(alpha + beta * x, sigma), obs=y)
+
+def nuts(model, **kwargs):
+    nuts_kernel = mcmc.NUTS(model, adapt_step_size = True)
+    return mcmc.MCMC(nuts_kernel, **kwargs)
+    
+
+def test_linear_regression():
+    # model = deepppl.DppplModel(model_file = 'deepppl/tests/good/linear_regression.stan')
+    #posterior = model.posterior(method=nuts, num_samples=3000, warmup_steps=300)
+    posterior = nuts(model, num_samples=1000, warmup_steps=200)
+
+    #hmc_posterior = MCMC(nuts_kernel, num_samples=1000, warmup_steps=200) \
+    #.run(is_cont_africa, ruggedness, log_gdp)
+    # Add Data
+    num_samples = 100
+    X = np.arange(num_samples)
+    y = np.arange(num_samples)
+    X = torch.Tensor(X)
+    y = torch.Tensor(y)
+    marginal = pyro.infer.EmpiricalMarginal(posterior.run(num_samples, X, y), sites=['alpha', 'beta', 'sigma'])    
+    samples_fstan = [marginal().item() for _ in range(3000)]
+    print(samples_fstan)
+
+test_linear_regression()
---
+++ deepppl/tests/target_py/linear_regression.py
@@ -0,0 +1,19 @@
+import torch
+from torch import tensor, randn
+import pyro
+import torch.distributions.constraints as constraints
+import pyro.distributions as dist
+
+
+def model(N, x, y):
+    ___shape = {}
+    ___shape['N'] = ()
+    ___shape['x'] = ()
+    ___shape['y'] = ()
+    ___shape['alpha'] = ()
+    ___shape['beta'] = ()
+    ___shape['sigma'] = ()
+    alpha = pyro.sample('alpha', ImproperUniform())
+    beta = pyro.sample('beta', ImproperUniform())
+    sigma = pyro.sample('sigma', LowerConstrainedImproperUniform(0.0))
+    pyro.sample('y' + '1', dist.normal(alpha + beta * x, sigma), obs=y)
