c821cb9753af9b6ec02b4fed07fea8d35b20a1e4
Guillaume Baudart <guillaume.baudart@ibm.com>
2019-02-06 22:29:29 -0500

    add LogisticNormal to known distribution

---
+++ deepppl/tests/target_py/gradient_warn.py
@@ -0,0 +1,22 @@
+import torch
+from torch import tensor, randn
+import pyro
+import torch.distributions.constraints as constraints
+import pyro.distributions as dist
+
+
+def model(N, Ngrps, grp_index, p):
+    ___shape = {}
+    ___shape['N'] = ()
+    ___shape['p'] = ()
+    ___shape['Ngrps'] = ()
+    ___shape['grp_index'] = N
+    ___shape['sigmaGrp'] = ()
+    ___shape['muGrp'] = ()
+    sigmaGrp = pyro.sample('sigmaGrp', dist.Uniform(0.0001, 100.0))
+    muGrp = pyro.sample('muGrp', dist.Uniform(-100, 1000.0))
+    ___shape['grpi'] = ()
+    for i in range(1, N + 1):
+        grpi = grp_index[i - 1]
+        pyro.sample('p' + '{}'.format(i - 1) + '1', dist.LogisticNormal(
+            muGrp[grpi - 1], sigmaGrp[grpi - 1]), obs=p[i - 1])
