e482f5bd9c472f33c8b1828c7d684d6c4615c365
Guillaume Baudart <guillaume.baudart@gmail.com>
2019-02-07 12:01:24 -0500

    Hotfix categorical_logit (#44)

--- deepppl/utils/utils.py
+++ deepppl/utils/utils.py
@@ -18,37 +18,41 @@ from pyro import distributions as dist
 from torch.distributions import constraints
 import torch
 
-## Utils to be imported by DppplModel
+# Utils to be imported by DppplModel
 
-def CategoricalLogits(logits):
+
+def categorical_logits(logits):
     return dist.Categorical(logits=logits)
+
 
 def bernoulli_logit(logits):
     return dist.Bernoulli(logits=logits)
 
+
 class ImproperUniform(dist.Normal):
-    def __init__(self, shape = None):
+    def __init__(self, shape=None):
         zeros = torch.zeros(shape) if shape else 0
         ones = torch.ones(shape) if shape else 1
         super(ImproperUniform, self).__init__(zeros, ones)
-        
+
     def log_prob(self, x):
         return x.new_zeros(x.shape)
 
+
 class LowerConstrainedImproperUniform(ImproperUniform):
-    def __init__(self, lower_bound = 0, shape = None):
+    def __init__(self, lower_bound=0, shape=None):
         super(LowerConstrainedImproperUniform, self).__init__(shape)
         self.support = constraints.greater_than(lower_bound)
 
+
 class UpperConstrainedImproperUniform(ImproperUniform):
-    def __init__(self, upper_bound = 0, shape = None):
+    def __init__(self, upper_bound=0, shape=None):
         super(UpperConstrainedImproperUniform, self).__init__(shape)
         self.support = constraints.less_than(upper_bound)
 
-    
 
-hooks = { x.__name__ : x for x in [
-    CategoricalLogits,
+hooks = {x.__name__: x for x in [
+    categorical_logits,
     ImproperUniform,
     LowerConstrainedImproperUniform,
     UpperConstrainedImproperUniform]
