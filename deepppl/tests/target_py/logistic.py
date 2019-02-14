878f6afa8cc1e877b5c9b87cc9faa151b2928bb5
Louis Mandel <lmandel@us.ibm.com>
2019-02-14 10:54:43 -0500

    Update expected result.

--- deepppl/tests/target_py/logistic.py
+++ deepppl/tests/target_py/logistic.py
@@ -11,8 +11,8 @@ def model(M=None, N=None, x=None, y=None):
     ___shape['M'] = ()
     ___shape['y'] = N
     ___shape['x'] = N, M
-    ___shape['beta'] = ()
-    beta = pyro.sample('beta', ImproperUniform())
+    ___shape['beta'] = M
+    beta = pyro.sample('beta', ImproperUniform(M))
     for m in range(1, M + 1):
         pyro.sample('beta' + '{}'.format(m - 1) + '1', dist.Cauchy(0.0, 2.5
             ), obs=beta[m - 1])
