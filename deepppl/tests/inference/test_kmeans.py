55b0d503791d5bb22a57e4be68488e87370ba4de
kakate@us.ibm.com <kakate@us.ibm.com>
2019-02-14 16:53:09 -0500

    Adding comparison to pystan

--- deepppl/tests/inference/test_kmeans.py
+++ deepppl/tests/inference/test_kmeans.py
@@ -11,7 +11,13 @@ from pyro.infer import mcmc
 import deepppl
 import os
 import pandas as pd
+import time
+import pystan
+from sklearn.datasets import make_blobs
 
+stan_model_file = 'deepppl/tests/good/kmeans.stan'
+global_num_iterations=3000
+global_num_chains=1
 
 def nuts(model, **kwargs):
     nuts_kernel = mcmc.NUTS(model, adapt_step_size=True)
@@ -26,6 +32,19 @@ def test_kmeans():
     X = torch.tensor([[1., 2.], [1., 4.], [1., 0.], [4., 2.], [4., 4.], [4., 0.]])
     num_clusters = 2
 
+    n_samples = 1500
+    random_state = 170
+    #X, y = make_blobs(n_samples=n_samples, random_state=random_state)
+    #X = torch.from_numpy(X)
+
+    data = {'N': num_samples,
+            'D': num_features,
+            'K': num_clusters,
+            'y': X}
+
+    #X = torch.tensor([[1., 1., 1., 4., 4., 4.], [2., 4., 0., 2., 4., 0.]])
+    #import pdb;pdb.set_trace()
+
     posterior = model.posterior(
         method=nuts,
         num_samples=30,
@@ -36,11 +55,59 @@ def test_kmeans():
                       transformed_data=model._transformed_data(N=num_samples, D=num_features, K=num_clusters, y=X)),
         sites="mu")
 
-    series = pd.Series([marginal()
-                        for _ in range(3000)], name=r'$\mu$')
-    print(series)
+    samples_fstan = [marginal()
+                        for _ in range(3000)]
+    stack_samples = torch.stack(samples_fstan)  
+    import pdb;pdb.set_trace()                      
+    #print(stack_samples.size())
+    compare_with_stan_output(data)
     # assert np.abs(series.mean() - 1000) < 1
     # assert np.abs(series.std() - 1.0) < 0.1
+
+def compare_with_stan_output(data):
+    #stan_code = open(stan_model_file).read()
+    t1 = time.time()
+    stan_code = """
+        data {
+        int<lower=0> N;  // number of data points
+        int<lower=1> D;  // number of dimensions
+        int<lower=1> K;  // number of clusters
+        vector[D] y[N];  // observations
+        }
+        transformed data {
+        real<upper=0> neg_log_K;
+        neg_log_K = -log(K);
+        }
+        parameters {
+        vector[D] mu[K]; // cluster means
+        }
+        transformed parameters {
+        real<upper=0> soft_z[N, K]; // log unnormalized clusters
+        for (n in 1:N)
+            for (k in 1:K)
+            soft_z[n, k] = neg_log_K
+                - 0.5 * dot_self(mu[k] - y[n]);
+        }
+        model {
+        // prior
+        for (k in 1:K)
+                mu[k] ~ normal(0, 1);
+        // likelihood
+        for (n in 1:N)
+                target += log_sum_exp(soft_z[n]);
+        }
+    """
+    data['y'] = np.array([[1., 2], [1.,4], [1, 0], [4.,2], [4., 4.],[4, 0]])# [2., 4., 0., 2., 4., 0.]])
+    data['K'] = 2
+    print(data)
+    # Compile and fit
+    sm1 = pystan.StanModel(model_code=str(stan_code))
+    fit_stan = sm1.sampling(data=data, iter=global_num_iterations, chains=global_num_chains, warmup = 300)
+
+    mu = fit_stan.extract(permuted=True)['mu']
+    print(mu.shape)
+    t2 = time.time()
+
 
 
 if __name__ == "__main__":
