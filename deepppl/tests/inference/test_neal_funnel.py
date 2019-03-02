fcf5a12fe9af0da2d18a894ca24399c4ef354067
Louis Mandel <lmandel@us.ibm.com>
2019-03-01 19:27:45 -0500

    Use generated_quantities function in neal_funnel

--- deepppl/tests/inference/test_neal_funnel.py
+++ deepppl/tests/inference/test_neal_funnel.py
@@ -1,25 +1,25 @@
-import torch
-from torch import tensor, rand
-import pyro
-import torch.distributions.constraints as constraints
-import pyro.distributions as dist
-from deepppl.utils.utils import *
-from torch import exp
+# import torch
+# from torch import tensor, rand
+# import pyro
+# import torch.distributions.constraints as constraints
+# import pyro.distributions as dist
+# from deepppl.utils.utils import *
+# from torch import exp
 
 
-def model2():
-    ___shape = {}
-    ___shape['y_std'] = ()
-    ___shape['x_std'] = ()
-    y_std = pyro.sample('y_std', ImproperUniform())
-    x_std = pyro.sample('x_std', ImproperUniform())
-    ___shape['y'] = ()
-    y = 3.0 * y_std
-    ___shape['x'] = ()
-    x = exp(y / 2) * x_std
-    pyro.sample('y_std' + '1', dist.Normal(0, 1), obs=y_std)
-    pyro.sample('x_std' + '2', dist.Normal(0, 1), obs=x_std)
-    return (x, y)
+# def model2():
+#     ___shape = {}
+#     ___shape['y_std'] = ()
+#     ___shape['x_std'] = ()
+#     y_std = pyro.sample('y_std', ImproperUniform())
+#     x_std = pyro.sample('x_std', ImproperUniform())
+#     ___shape['y'] = ()
+#     y = 3.0 * y_std
+#     ___shape['x'] = ()
+#     x = exp(y / 2) * x_std
+#     pyro.sample('y_std' + '1', dist.Normal(0, 1), obs=y_std)
+#     pyro.sample('x_std' + '2', dist.Normal(0, 1), obs=x_std)
+#     return (x, y)
 
 
 import torch
@@ -54,7 +54,7 @@ def nuts(model, **kwargs):
 
 def test_neals_funnel():
     model = deepppl.DppplModel(model_file=stan_model_file)
-    model._model = model2
+    # model._model = model2
     posterior = model.posterior(
         method=nuts,
         num_samples=global_num_iterations-global_warmup_steps,
@@ -63,9 +63,16 @@ def test_neals_funnel():
     marginal = pyro.infer.EmpiricalMarginal(
         posterior.run(), sites=['x_std', 'y_std'])
 
-    xy = [model._model() for _ in range(100)]
-    x = [xi.item() for (xi, yi) in xy]
-    y = [yi.item() for (xi, yi) in xy]
+    # xy = [model._model() for _ in range(100)]
+    # x = [xi.item() for (xi, yi) in xy]
+    # y = [yi.item() for (xi, yi) in xy]
+
+    def params_of_sample(s):
+        return { 'x_std':s[0], 'y_std': s[1] }
+    samples_fstan = [marginal() for _ in range(100)]
+    xy = [ model.generated_quantities(parameters=params_of_sample(sample)) for sample in samples_fstan ]
+    x = [ o['x'].item() for o in xy ]
+    y = [ o['y'].item() for o in xy ]
 
     # samples_fstan = [marginal() for _ in range(100)]
     # o = model.generated_quantities()
