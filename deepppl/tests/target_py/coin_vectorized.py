c624cdaa59097324caac98d28095f5c9f9a99d8f
Avi Shinnar <shinnar@us.ibm.com>
2019-03-04 20:11:02 -0500

    Fix a bunch of test target_py/*.py files to match the new output (without __shapes)

--- deepppl/tests/target_py/coin_vectorized.py
+++ deepppl/tests/target_py/coin_vectorized.py
@@ -5,10 +5,7 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def model(x=None):
-    ___shape = {}
-    ___shape['x'] = 10
-    ___shape['theta'] = ()
-    theta = pyro.sample('theta', dist.Uniform(0.0, 1.0))
+def model(x: 'int[10]'=None):
+    theta: 'real' = pyro.sample('theta', dist.Uniform(0.0, 1.0))
     pyro.sample('theta' + '__1', dist.Uniform(0.0, 1.0), obs=theta)
     pyro.sample('x' + '__2', dist.Bernoulli(theta), obs=x)
