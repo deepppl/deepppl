24060d200d7922b5781e5f0a48b86941a504c0e4
Avi Shinnar <shinnar@us.ibm.com>
2019-03-06 17:25:11 -0500

    Tweak kmeans to replace vectors with arrays

--- deepppl/tests/target_py/kmeans.py
+++ deepppl/tests/target_py/kmeans.py
@@ -5,44 +5,35 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def transformed_data(D=None, K=None, N=None, y=None):
-    ___shape = {}
-    ___shape['neg_log_K'] = ()
-    neg_log_K = -log(K)
+def transformed_data(D: 'int'=None, K: 'int'=None, N: 'int'=None, y:
+    'real[N,D]'=None):
+    neg_log_K: 'real' = -log(K)
     return {'neg_log_K': neg_log_K}
 
-
-def model(D=None, K=None, N=None, y=None, transformed_data=None):
+def model(D: 'int'=None, K: 'int'=None, N: 'int'=None, y: 'real[N,D]'=None,
+    transformed_data=None):
     neg_log_K = transformed_data['neg_log_K']
-    ___shape = {}
-    ___shape['N'] = ()
-    ___shape['D'] = ()
-    ___shape['K'] = ()
-    ___shape['y'] = N, D
-    ___shape['mu'] = K, D
-    mu = pyro.sample('mu', ImproperUniform((K, D)))
-    ___shape['soft_z'] = N, K
-    soft_z = zeros(___shape['soft_z'])
+    mu: 'real[K,D]' = pyro.sample('mu', ImproperUniform(shape=(K, D)))
+    soft_z: 'real[N,K]' = zeros((N, K))
     for n in range(1, N + 1):
         for k in range(1, K + 1):
-            soft_z[n - 1, k - 1] = neg_log_K - 0.5 * dot_self(mu[k - 1] - y
-                                                              [n - 1])
+            soft_z[n - 1, k - 1]: 'real' = neg_log_K - 0.5 * dot_self(mu[k -
+                1] - y[n - 1])
     for k in range(1, K + 1):
-        pyro.sample('mu' + '__{}'.format(k - 1) + '__1', dist.Normal(zeros(D),
-                                                                 ones(D)), obs=mu[k - 1])
+        pyro.sample('mu' + '__{}'.format(k - 1) + '__1', dist.Normal(zeros(
+            D) * ones(D), ones(D) * ones(D)), obs=mu[k - 1])
     for n in range(1, N + 1):
-        pyro.sample('expr' + '__{}'.format(n) + '__2', dist.Exponential(1.0),
-                    obs=-log_sum_exp(soft_z[n - 1]))
+        pyro.sample('expr' + '__{}'.format(n) + '__2', dist.Exponential(1.0
+            ), obs=-log_sum_exp(soft_z[n - 1]))
 
-def generated_quantities(D=None, K=None, N=None, y=None, transformed_data=
-    None, parameters=None):
+
+def generated_quantities(D: 'int'=None, K: 'int'=None, N: 'int'=None, y:
+    'real[N,D]'=None, transformed_data=None, parameters=None):
     neg_log_K = transformed_data['neg_log_K']
     mu = parameters['mu']
-    ___shape = {}
-    ___shape['soft_z'] = N, K
-    soft_z = zeros(___shape['soft_z'])
+    soft_z: 'real[N,K]' = zeros((N, K))
     for n in range(1, N + 1):
         for k in range(1, K + 1):
-            soft_z[n - 1, k - 1] = neg_log_K - 0.5 * dot_self(mu[k - 1] - y
-                [n - 1])
+            soft_z[n - 1, k - 1]: 'real' = neg_log_K - 0.5 * dot_self(mu[k -
+                1] - y[n - 1])
     return {'soft_z': soft_z}
