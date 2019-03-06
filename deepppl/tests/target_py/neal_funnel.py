b1ecfdc7937ea70792be5da12875751568f86b20
Avi Shinnar <shinnar@us.ibm.com>
2019-03-05 21:19:12 -0500

    add support for transformed parameters

--- deepppl/tests/target_py/neal_funnel.py
+++ deepppl/tests/target_py/neal_funnel.py
@@ -6,24 +6,17 @@ import pyro.distributions as dist
 
 
 def model():
-    ___shape = {}
-    ___shape['y_std'] = ()
-    ___shape['x_std'] = ()
-    y_std = pyro.sample('y_std', ImproperUniform())
-    x_std = pyro.sample('x_std', ImproperUniform())
-    ___shape['y'] = ()
-    y = 3.0 * y_std
-    ___shape['x'] = ()
-    x = exp(y / 2) * x_std
+    y_std: 'real' = pyro.sample('y_std', ImproperUniform())
+    x_std: 'real' = pyro.sample('x_std', ImproperUniform())
+    y: 'real' = 3.0 * y_std
+    x: 'real' = exp(y / 2) * x_std
     pyro.sample('y_std' + '__1', dist.Normal(0, 1), obs=y_std)
     pyro.sample('x_std' + '__2', dist.Normal(0, 1), obs=x_std)
+
 
 def generated_quantities(parameters=None):
     x_std = parameters['x_std']
     y_std = parameters['y_std']
-    ___shape = {}
-    ___shape['y'] = ()
-    y = 3.0 * y_std
-    ___shape['x'] = ()
-    x = exp(y / 2) * x_std
+    y: 'real' = 3.0 * y_std
+    x: 'real' = exp(y / 2) * x_std
     return {'y': y, 'x': x}
