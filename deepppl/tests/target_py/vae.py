e48a2856447471daa707019ebdcaf34d7dfd9e08
Avi Shinnar <shinnar@us.ibm.com>
2019-03-07 12:54:56 -0500

    modify schools stan code to use a 1 dimensional vector for parameters

--- deepppl/tests/target_py/vae.py
+++ deepppl/tests/target_py/vae.py
@@ -38,3 +38,26 @@ def model(nz=None, x=None):
     pyro.sample('z' + '__1', dist.Normal(zeros(nz), ones(nz)), obs=z)
     mu = decoder(z)
     pyro.sample('x' + '__2', dist.Bernoulli(mu), obs=x)
+
+import torch
+from torch import tensor, rand
+import pyro
+import torch.distributions.constraints as constraints
+import pyro.distributions as dist
+
+
+def guide_(nz: 'int'=None, x: 'int[28,28]'=None):
+    pyro.module('encoder', encoder)
+    encoded: 'real[2,nz]' = encoder(x)
+    mu_z: 'real[nz]' = encoded[1 - 1]
+    sigma_z: 'real[nz]' = encoded[2 - 1]
+    z: 'real[nz][??]' = pyro.sample('z', dist.Normal(mu_z, sigma_z))
+
+
+def model(nz: 'int'=None, x: 'int[28,28]'=None):
+    pyro.module('decoder', decoder)
+    z: 'real[nz][??]' = pyro.sample('z', ImproperUniform(shape=nz))
+    mu: 'real[28,28]' = zeros((28, 28))
+    pyro.sample('z' + '__1', dist.Normal(0 * ones(nz), 1), obs=z)
+    mu: 'real[28,28]' = decoder(z)
+    pyro.sample('x' + '__2', dist.Bernoulli(mu), obs=x)
