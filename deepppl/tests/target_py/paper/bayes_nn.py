a10c31de32ef7eb41c76dc771d27bcd9c3cefd2c
Avi Shinnar <shinnar@us.ibm.com>
2019-03-08 06:56:35 -0500

    Update the paper expected outputs.  Change them to not use type annotations, since we are likely ommitting them from the paper

--- deepppl/tests/target_py/paper/bayes_nn.py
+++ deepppl/tests/target_py/paper/bayes_nn.py
@@ -5,38 +5,38 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def guide_mlp(img: 'int[28,28]'=None, label: 'int'=None):
+def guide_mlp(img=None, label=None):
     guide_mlp = {}
-    w1_mu: 'real' = pyro.param('w1_mu', (2 - -2) * rand(()) + -2)
-    w1_sgma: 'real' = pyro.param('w1_sgma', (2 - -2) * rand(()) + -2)
-    b1_mu: 'real' = pyro.param('b1_mu', (2 - -2) * rand(()) + -2)
-    b1_sgma: 'real' = pyro.param('b1_sgma', (2 - -2) * rand(()) + -2)
-    w2_mu: 'real' = pyro.param('w2_mu', (2 - -2) * rand(()) + -2)
-    w2_sgma: 'real' = pyro.param('w2_sgma', (2 - -2) * rand(()) + -2)
-    b2_mu: 'real' = pyro.param('b2_mu', (2 - -2) * rand(()) + -2)
-    b2_sgma: 'real' = pyro.param('b2_sgma', (2 - -2) * rand(()) + -2)
-    guide_mlp['l1.weight']: 'real' = dist.Normal(w1_mu, exp(w1_sgma))
-    guide_mlp['l1.bias']: 'real' = dist.Normal(b1_mu, exp(b1_sgma))
-    guide_mlp['l2.weight']: 'real' = dist.Normal(w2_mu, exp(w2_sgma))
-    guide_mlp['l2.bias']: 'real' = dist.Normal(b2_mu, exp(b2_sgma))
+    w1_mu = pyro.param('w1_mu', (2 - -2) * rand(()) + -2)
+    w1_sgma = pyro.param('w1_sgma', (2 - -2) * rand(()) + -2)
+    b1_mu = pyro.param('b1_mu', (2 - -2) * rand(()) + -2)
+    b1_sgma = pyro.param('b1_sgma', (2 - -2) * rand(()) + -2)
+    w2_mu = pyro.param('w2_mu', (2 - -2) * rand(()) + -2)
+    w2_sgma = pyro.param('w2_sgma', (2 - -2) * rand(()) + -2)
+    b2_mu = pyro.param('b2_mu', (2 - -2) * rand(()) + -2)
+    b2_sgma = pyro.param('b2_sgma', (2 - -2) * rand(()) + -2)
+    guide_mlp['l1.weight'] = dist.Normal(w1_mu, exp(w1_sgma))
+    guide_mlp['l1.bias'] = dist.Normal(b1_mu, exp(b1_sgma))
+    guide_mlp['l2.weight'] = dist.Normal(w2_mu, exp(w2_sgma))
+    guide_mlp['l2.bias'] = dist.Normal(b2_mu, exp(b2_sgma))
     lifted_mlp = pyro.random_module('mlp', mlp, guide_mlp)
     return lifted_mlp()
 
 
-def prior_mlp(img: 'int[28,28]'=None, label: 'int'=None):
+def prior_mlp(img=None, label=None):
     prior_mlp = {}
-    prior_mlp['l1.weight']: 'real' = ImproperUniform(shape=mlp.l1.weight.shape)
-    prior_mlp['l1.bias']: 'real' = ImproperUniform(shape=mlp.l1.bias.shape)
-    prior_mlp['l2.weight']: 'real' = ImproperUniform(shape=mlp.l2.weight.shape)
-    prior_mlp['l2.bias']: 'real' = ImproperUniform(shape=mlp.l2.bias.shape)
+    prior_mlp['l1.weight'] = ImproperUniform(shape=mlp.l1.weight.shape)
+    prior_mlp['l1.bias'] = ImproperUniform(shape=mlp.l1.bias.shape)
+    prior_mlp['l2.weight'] = ImproperUniform(shape=mlp.l2.weight.shape)
+    prior_mlp['l2.bias'] = ImproperUniform(shape=mlp.l2.bias.shape)
     lifted_mlp = pyro.random_module('mlp', mlp, prior_mlp)
     return lifted_mlp()
 
 
-def model(img: 'int[28,28]'=None, label: 'int'=None):
+def model(img=None, label=None):
     mlp = prior_mlp(img, label)
     model_mlp = mlp.state_dict()
-    logits: 'real[10]' = zeros(10)
+    logits = zeros(10)
     pyro.sample('model_mlp' + '__{}'.format('l1.weight') + '__1', dist.
         Normal(0, 1), obs=model_mlp['l1.weight'])
     pyro.sample('model_mlp' + '__{}'.format('l1.bias') + '__2', dist.Normal
@@ -45,5 +45,5 @@ def model(img: 'int[28,28]'=None, label: 'int'=None):
         Normal(0, 1), obs=model_mlp['l2.weight'])
     pyro.sample('model_mlp' + '__{}'.format('l2.bias') + '__4', dist.Normal
         (0, 1), obs=model_mlp['l2.bias'])
-    logits: 'real[10]' = mlp(img)
+    logits = mlp(img)
     pyro.sample('label' + '__5', categorical_logits(logits), obs=label)
--- deepppl/tests/target_py/paper/coin.py
+++ deepppl/tests/target_py/paper/coin.py
@@ -5,7 +5,7 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def model(N: 'int'=None, x: 'int[N]'=None):
-    z: 'real' = pyro.sample('z', dist.Uniform(0.0, 1.0))
+def model(N=None, x=None):
+    z = pyro.sample('z', dist.Uniform(0.0, 1.0))
     pyro.sample('z' + '__1', dist.Beta(1, 1), obs=z)
-    pyro.sample('x' + '__2', dist.Bernoulli(z), obs=x)
+    pyro.sample('x' + '__2', dist.Bernoulli(z * ones(N)), obs=x)
