03c5be9cbd294f29f70341d8c093542acabcf0c9
Avi Shinnar <shinnar@us.ibm.com>
2019-03-08 10:13:04 -0500

    update mlp output

--- deepppl/tests/target_py/mlp.py
+++ deepppl/tests/target_py/mlp.py
@@ -5,68 +5,68 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def guide_mlp(batch_size=None, imgs=None, labels=None):
-    ___shape = {}
-    ___shape['batch_size'] = ()
-    ___shape['imgs'] = 28, 28, batch_size
-    ___shape['labels'] = batch_size
-    ___shape['l1wloc'] = mlp.l1.weight.shape
-    ___shape['l1wscale'] = mlp.l1.weight.shape
-    ___shape['l1bloc'] = mlp.l1.bias.shape
-    ___shape['l1bscale'] = mlp.l1.bias.shape
-    ___shape['l2wloc'] = mlp.l2.weight.shape
-    ___shape['l2wscale'] = mlp.l2.weight.shape
-    ___shape['l2bloc'] = mlp.l2.bias.shape
-    ___shape['l2bscale'] = mlp.l2.bias.shape
+def guide_mlp(batch_size: 'int'=None, imgs: 'int[28,28,batch_size]'=None,
+    labels: 'int[batch_size]'=None):
     guide_mlp = {}
-    l1wloc = pyro.param('l1wloc', randn(___shape['l1wloc']))
-    l1wscale = pyro.param('l1wscale', randn(___shape['l1wscale']))
-    guide_mlp['l1.weight'] = dist.Normal(l1wloc, softplus(l1wscale))
-    l1bloc = pyro.param('l1bloc', randn(___shape['l1bloc']))
-    l1bscale = pyro.param('l1bscale', randn(___shape['l1bscale']))
-    guide_mlp['l1.bias'] = dist.Normal(l1bloc, softplus(l1bscale))
-    l2wloc = pyro.param('l2wloc', randn(___shape['l2wloc']))
-    l2wscale = pyro.param('l2wscale', randn(___shape['l2wscale']))
-    guide_mlp['l2.weight'] = dist.Normal(l2wloc, softplus(l2wscale))
-    l2bloc = pyro.param('l2bloc', randn(___shape['l2bloc']))
-    l2bscale = pyro.param('l2bscale', randn(___shape['l2bscale']))
-    guide_mlp['l2.bias'] = dist.Normal(l2bloc, softplus(l2bscale))
+    l1wloc: 'real[mlp.l1.weight$shape]' = pyro.param('l1wloc', randn(mlp.l1
+        .weight.size()))
+    l1wscale: 'real[mlp.l1.weight$shape]' = pyro.param('l1wscale', randn(
+        mlp.l1.weight.size()))
+    guide_mlp['l1.weight']: 'real[mlp.l1.weight$shape]' = dist.Normal(l1wloc,
+        softplus(l1wscale))
+    l1bloc: 'real[mlp.l1.bias$shape]' = pyro.param('l1bloc', randn(mlp.l1.
+        bias.size()))
+    l1bscale: 'real[mlp.l1.bias$shape]' = pyro.param('l1bscale', randn(mlp.
+        l1.bias.size()))
+    guide_mlp['l1.bias']: 'real[mlp.l1.bias$shape]' = dist.Normal(l1bloc,
+        softplus(l1bscale))
+    l2wloc: 'real[mlp.l2.weight$shape]' = pyro.param('l2wloc', randn(mlp.l2
+        .weight.size()))
+    l2wscale: 'real[mlp.l2.weight$shape]' = pyro.param('l2wscale', randn(
+        mlp.l2.weight.size()))
+    guide_mlp['l2.weight']: 'real[mlp.l2.weight$shape]' = dist.Normal(l2wloc,
+        softplus(l2wscale))
+    l2bloc: 'real[mlp.l2.bias$shape]' = pyro.param('l2bloc', randn(mlp.l2.
+        bias.size()))
+    l2bscale: 'real[mlp.l2.bias$shape]' = pyro.param('l2bscale', randn(mlp.
+        l2.bias.size()))
+    guide_mlp['l2.bias']: 'real[mlp.l2.bias$shape]' = dist.Normal(l2bloc,
+        softplus(l2bscale))
     lifted_mlp = pyro.random_module('mlp', mlp, guide_mlp)
     return lifted_mlp()
 
 
-def prior_mlp(batch_size=None, imgs=None, labels=None):
-    ___shape = {}
-    ___shape['batch_size'] = ()
-    ___shape['imgs'] = 28, 28, batch_size
-    ___shape['labels'] = batch_size
+def prior_mlp(batch_size: 'int'=None, imgs: 'int[28,28,batch_size]'=None,
+    labels: 'int[batch_size]'=None):
     prior_mlp = {}
-    prior_mlp['l1.weight'] = ImproperUniform(mlp.l1.weight.shape)
-    prior_mlp['l1.bias'] = ImproperUniform(mlp.l1.bias.shape)
-    prior_mlp['l2.weight'] = ImproperUniform(mlp.l2.weight.shape)
-    prior_mlp['l2.bias'] = ImproperUniform(mlp.l2.bias.shape)
+    prior_mlp['l1.weight']: 'real[mlp.l1.weight$shape]' = ImproperUniform(shape
+        =mlp.l1.weight.shape)
+    prior_mlp['l1.bias']: 'real[mlp.l1.bias$shape]' = ImproperUniform(shape
+        =mlp.l1.bias.shape)
+    prior_mlp['l2.weight']: 'real[mlp.l2.weight$shape]' = ImproperUniform(shape
+        =mlp.l2.weight.shape)
+    prior_mlp['l2.bias']: 'real[mlp.l2.bias$shape]' = ImproperUniform(shape
+        =mlp.l2.bias.shape)
     lifted_mlp = pyro.random_module('mlp', mlp, prior_mlp)
     return lifted_mlp()
 
 
-def model(batch_size=None, imgs=None, labels=None):
-    ___shape = {}
-    ___shape['batch_size'] = ()
-    ___shape['imgs'] = 28, 28, batch_size
-    ___shape['labels'] = batch_size
+def model(batch_size: 'int'=None, imgs: 'int[28,28,batch_size]'=None,
+    labels: 'int[batch_size]'=None):
     mlp = prior_mlp(batch_size, imgs, labels)
     model_mlp = mlp.state_dict()
-    ___shape['logits'] = batch_size
-    logits = zeros(___shape['logits'])
-    pyro.sample('model_mlp' + '__{}'.format('l1.weight') + '__1', dist.Normal(
-        zeros(mlp.l1.weight.shape), ones(mlp.l1.weight.shape)), obs=model_mlp['l1.weight'])
-    pyro.sample('model_mlp' + '__{}'.format('l1.bias') + '__2', dist.Normal(
-        zeros(mlp.l1.bias.shape), ones(mlp.l1.bias.shape)), obs=model_mlp[
+    logits: 'real[batch_size]' = zeros(batch_size)
+    pyro.sample('model_mlp' + '__{}'.format('l1.weight') + '__1', dist.
+        Normal(zeros(mlp.l1.weight.shape), ones(mlp.l1.weight.shape)), obs=
+        model_mlp['l1.weight'])
+    pyro.sample('model_mlp' + '__{}'.format('l1.bias') + '__2', dist.Normal
+        (zeros(mlp.l1.bias.shape), ones(mlp.l1.bias.shape)), obs=model_mlp[
         'l1.bias'])
-    pyro.sample('model_mlp' + '__{}'.format('l2.weight') + '__3', dist.Normal(
-        zeros(mlp.l2.weight.shape), ones(mlp.l2.weight.shape)), obs=model_mlp['l2.weight'])
-    pyro.sample('model_mlp' + '__{}'.format('l2.bias') + '__4', dist.Normal(
-        zeros(mlp.l2.bias.shape), ones(mlp.l2.bias.shape)), obs=model_mlp[
+    pyro.sample('model_mlp' + '__{}'.format('l2.weight') + '__3', dist.
+        Normal(zeros(mlp.l2.weight.shape), ones(mlp.l2.weight.shape)), obs=
+        model_mlp['l2.weight'])
+    pyro.sample('model_mlp' + '__{}'.format('l2.bias') + '__4', dist.Normal
+        (zeros(mlp.l2.bias.shape), ones(mlp.l2.bias.shape)), obs=model_mlp[
         'l2.bias'])
-    logits = mlp(imgs)
+    logits: 'real[batch_size]' = mlp(imgs)
     pyro.sample('labels' + '__5', categorical_logits(logits), obs=labels)
