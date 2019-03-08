f229e4ebe41a094158145d7dfce4b20a03dfbe15
Avi Shinnar <shinnar@us.ibm.com>
2019-03-08 08:23:51 -0500

    Fix some tests

--- deepppl/tests/target_py/coin.py
+++ deepppl/tests/target_py/coin.py
@@ -5,7 +5,9 @@ import torch.distributions.constraints as constraints
 import pyro.distributions as dist
 
 
-def model(N=None, x=None):
-    z = pyro.sample('z', dist.Uniform(0.0, 1.0))
-    pyro.sample('z' + '__1', dist.Beta(1, 1), obs=z)
-    pyro.sample('x' + '__2', dist.Bernoulli(z * ones(N)), obs=x)
+def model(x: 'int[10]'=None):
+    theta: 'real' = pyro.sample('theta', dist.Uniform(0.0, 1.0))
+    pyro.sample('theta' + '__1', dist.Uniform(0, 1), obs=theta)
+    for i in range(1, 10 + 1):
+        pyro.sample('x' + '__{}'.format(i - 1) + '__2', dist.Bernoulli(
+            theta), obs=x[i - 1])
