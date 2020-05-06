b284a9c14c2a5fb32c09206fa8d3eac2342a2104
Guillaume Baudart <guillaume.baudart@ibm.com>
2020-05-06 18:09:08 -0400

    Clean up test

---
+++ deepppl/tests/target_py/mlp_default_init.py
@@ -0,0 +1,54 @@
+import torch
+from torch import tensor, rand
+import pyro
+import torch.distributions.constraints as constraints
+import pyro.distributions as dist
+
+
+def guide_mlp(batch_size=None, imgs=None, labels=None):
+    guide_mlp = {}
+    l1wloc = pyro.param('l1wloc', (2 - -2) * rand(mlp.l1.weight.size()) + -2)
+    l1wscale = pyro.param('l1wscale', (2 - -2) * rand(mlp.l1.weight.size()) +
+        -2)
+    guide_mlp['l1.weight'] = dist.Normal(l1wloc, softplus(l1wscale))
+    l1bloc = pyro.param('l1bloc', randn(mlp.l1.bias.size()))
+    l1bscale = pyro.param('l1bscale', randn(mlp.l1.bias.size()))
+    guide_mlp['l1.bias'] = dist.Normal(l1bloc, softplus(l1bscale))
+    l2wloc = pyro.param('l2wloc', randn(mlp.l2.weight.size()))
+    l2wscale = pyro.param('l2wscale', randn(mlp.l2.weight.size()))
+    guide_mlp['l2.weight'] = dist.Normal(l2wloc, softplus(l2wscale))
+    l2bloc = pyro.param('l2bloc', randn(mlp.l2.bias.size()))
+    l2bscale = pyro.param('l2bscale', randn(mlp.l2.bias.size()))
+    guide_mlp['l2.bias'] = dist.Normal(l2bloc, softplus(l2bscale))
+    lifted_mlp = pyro.random_module('mlp', mlp, guide_mlp)
+    return lifted_mlp()
+
+
+def prior_mlp(batch_size=None, imgs=None, labels=None):
+    prior_mlp = {}
+    prior_mlp['l1.weight'] = ImproperUniform(shape=mlp.l1.weight.shape)
+    prior_mlp['l1.bias'] = ImproperUniform(shape=mlp.l1.bias.shape)
+    prior_mlp['l2.weight'] = ImproperUniform(shape=mlp.l2.weight.shape)
+    prior_mlp['l2.bias'] = ImproperUniform(shape=mlp.l2.bias.shape)
+    lifted_mlp = pyro.random_module('mlp', mlp, prior_mlp)
+    return lifted_mlp()
+
+
+def model(batch_size=None, imgs=None, labels=None):
+    mlp = prior_mlp(batch_size, imgs, labels)
+    model_mlp = dict(mlp.named_parameters())
+    logits = zeros(batch_size)
+    sample('model_mlp' + '__{}'.format('l1.weight') + '__1', dist.
+        Normal(zeros(mlp.l1.weight.shape), ones(mlp.l1.weight.shape)), obs=
+        model_mlp['l1.weight'])
+    sample('model_mlp' + '__{}'.format('l1.bias') + '__2', dist.Normal
+        (zeros(mlp.l1.bias.shape), ones(mlp.l1.bias.shape)), obs=model_mlp[
+        'l1.bias'])
+    sample('model_mlp' + '__{}'.format('l2.weight') + '__3', dist.
+        Normal(zeros(mlp.l2.weight.shape), ones(mlp.l2.weight.shape)), obs=
+        model_mlp['l2.weight'])
+    sample('model_mlp' + '__{}'.format('l2.bias') + '__4', dist.Normal
+        (zeros(mlp.l2.bias.shape), ones(mlp.l2.bias.shape)), obs=model_mlp[
+        'l2.bias'])
+    logits = mlp(imgs)
+    sample('labels' + '__5', categorical_logits(logits), obs=labels)
--- deepppl/tests/test_irl.py
+++ deepppl/tests/test_irl.py
@@ -56,336 +56,110 @@ def normalize_and_compare(src_file, target_file, verbose=True):
     compiled = dpplc.stan2astpyFile(src_file, config, verbose)
     assert code_to_normalized(compiled) == target
 
-
-def test_coin():
-    filename = r'deepppl/tests/good/coin.stan'
-    target_file = r'deepppl/tests/target_py/coin.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_gaussian():
-    filename = r'deepppl/tests/good/gaussian.stan'
-    target_file = r'deepppl/tests/target_py/gaussian.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_gaussian_log_density():
-    filename = r'deepppl/tests/good/gaussian_log_density.stan'
-    target_file = r'deepppl/tests/target_py/gaussian_log_density.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_double_gaussian():
-    filename = r'deepppl/tests/good/double_gaussian.stan'
-    target_file = r'deepppl/tests/target_py/double_gaussian.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_log_normal():
-    filename = r'deepppl/tests/good/log_normal.stan'
-    target_file = r'deepppl/tests/target_py/log_normal.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_operators():
-    filename = r'deepppl/tests/good/operators.stan'
-    target_file = r'deepppl/tests/target_py/operators.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_operators_expr():
-    filename = r'deepppl/tests/good/operators-expr.stan'
-    target_file = r'deepppl/tests/target_py/operators-expr.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_coin_vectorized():
-    filename = r'deepppl/tests/good/coin_vectorized.stan'
-    target_file = r'deepppl/tests/target_py/coin_vectorized.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="Type inference does not currently support promoting an integer to a float")
-def test_coin_transformed_data():
-    filename = r'deepppl/tests/good/coin_transformed_data.stan'
-    target_file = r'deepppl/tests/target_py/coin_transformed_data.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=True)
-def test_coin_reverted_lines():
-    """Inside a `block`, stan semantics do not requires lines to be
-    ordered.
-    """
-    filename = r'deepppl/tests/good/coin_reverted.stan'
-    target_file = r'deepppl/tests/target_py/coin_vectorized.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_coin_guide():
-    filename = r'deepppl/tests/good/coin_guide.stan'
-    target_file = r'deepppl/tests/target_py/coin_guide.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_coin_guide_init():
-    filename = r'deepppl/tests/good/coin_guide_init.stan'
-    target_file = r'deepppl/tests/target_py/coin_guide_init.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_simple_init():
-    filename = r'deepppl/tests/good/simple_init.stan'
-    target_file = r'deepppl/tests/target_py/simple_init.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_lstm():
-    filename = r'deepppl/tests/good/lstm.stan'
-    target_file = r'deepppl/tests/target_py/lstm.py'
-    normalize_and_compare(filename, target_file)
-
 def compile(filename):
     config = dpplc.Config()
     dpplc.stan2astpyFile(filename, config, verbose=True)
 
-def test_coin_guide_missing_var():
-    with pytest.raises(MissingGuideException):
-        filename = r'deepppl/tests/good/coin_guide_missing_var.stan'
-        compile(filename)
-
-
-def test_coin_guide_sample_obs():
-    with pytest.raises(ObserveOnGuideException):
-        filename = r'deepppl/tests/good/coin_guide_sample_obs.stan'
-        compile(filename)
-
-
-def test_coin_guide_missing_model():
-    "Implicit prior allows to write missing model."
-    with not_raises(MissingModelException):
-        filename = r'deepppl/tests/good/coin_guide_missing_model.stan'
-        compile(filename)
-
-
-def test_mlp_undeclared_parameters():
-    with pytest.raises(UndeclaredParametersException):
-        filename = r'deepppl/tests/good/mlp_undeclared_parameters.stan'
-        compile(filename)
-
-
-def test_mlp_undeclared_network1():
-    with pytest.raises(UndeclaredNetworkException):
-        filename = r'deepppl/tests/good/mlp_undeclared_net1.stan'
-        compile(filename)
-
-
-def test_mlp_undeclared_network2():
-    with pytest.raises(UndeclaredNetworkException):
-        filename = r'deepppl/tests/good/mlp_undeclared_net2.stan'
-        compile(filename)
-
-
-def test_mlp_missing_guide():
-    with pytest.raises(MissingGuideNetException):
-        filename = r'deepppl/tests/good/mlp_missing_guide.stan'
-        compile(filename)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_mlp_missing_model():
-    with not_raises(MissingPriorNetException):
-        filename = r'deepppl/tests/good/mlp_missing_model.stan'
-        compile(filename)
-
-
-@pytest.mark.xfail(strict=False, reason="Type inference results in a different error.")
-def test_mlp_incorrect_shape1():
-    with pytest.raises(IncompatibleShapes):
-        filename = r'deepppl/tests/good/mlp_incorrect_shape1.stan'
-        compile(filename)
-
-
-@pytest.mark.xfail(strict=False, reason="Type inference results in this test passing.")
-def test_mlp_incorrect_shape2():
-    with pytest.raises(IncompatibleShapes):
-        filename = r'deepppl/tests/good/mlp_incorrect_shape2.stan'
-        compile(filename)
-
-
-@pytest.mark.xfail(strict=False, reason="Type inference results in this test passing.")
-def test_mlp_incorrect_shape3():
-    with pytest.raises(IncompatibleShapes):
-        filename = r'deepppl/tests/good/mlp_incorrect_shape3.stan'
-        compile(filename)
-
-
-@pytest.mark.xfail(strict=False, reason="Type inference results in this test passing.")
-def test_mlp_incorrect_shape4():
-    with pytest.raises(IncompatibleShapes):
-        filename = r'deepppl/tests/good/mlp_incorrect_shape4.stan'
-        compile(filename)
-
-
-def test_coin_invalid_sampling():
-    with not_raises(InvalidSamplingException):
-        filename = r'deepppl/tests/good/coin_invalid_sampling.stan'
-        compile(filename)
-
-
-def test_coin_invalid_sampling2():
-    with not_raises(NonRandomSamplingException):
-        filename = r'deepppl/tests/good/coin_invalid_sampling2.stan'
-        compile(filename)
-
-
-def test_coin_unknown_identifier():
-    with pytest.raises(UndeclaredVariableException):
-        filename = r'deepppl/tests/good/coin_unknown_identifier.stan'
-        compile(filename)
-
-
-def test_coin_unknown_distribution():
-    with pytest.raises(UnknownDistributionException):
-        filename = r'deepppl/tests/good/coin_unknown_distribution.stan'
-        compile(filename)
-
-
-def test_coin_already_declared():
-    with pytest.raises(AlreadyDeclaredException):
-        filename = r'deepppl/tests/good/coin_already_declared_var.stan'
-        compile(filename)
-
-
-def test_mlp_unsupported_property():
-    with pytest.raises(UnsupportedProperty):
-        filename = r'deepppl/tests/good/mlp_unsupported_prop.stan'
-        target_file = r'deepppl/tests/target_py/mlp.py'
-        normalize_and_compare(filename, target_file)
-
-
-def test_mlp_unsupported_property1():
-    with pytest.raises(UnsupportedProperty):
-        filename = r'deepppl/tests/good/mlp_unsupported_prop1.stan'
-        target_file = r'deepppl/tests/target_py/mlp.py'
-        normalize_and_compare(filename, target_file)
-
-
-def test_mlp():
-    filename = r'deepppl/tests/good/mlp.stan'
-    target_file = r'deepppl/tests/target_py/mlp.py'
+ 
+compile_tests = [
+    ('coin', None), 
+    ('coin_vectorized', None), 
+    ('coin_guide', None), 
+    ('coin_guide_init', None), 
+    ('gaussian', None), 
+    ('gaussian_log_density', None), 
+    ('double_gaussian', None), 
+    ('log_normal', None), 
+    ('operators', None), 
+    ('operators-expr', None),
+    ('simple_init', None), 
+    ('vae', None),
+    ('missing_data', None),
+    ('neal_funnel', None), 
+    ('cockroaches', None), 
+    ('seeds', None),
+    ('coin_reverted', 'In stan lines are not ordered'),
+    ('coin_transformed_data', 'Type Inference'), 
+    ('lstm', 'Type inference'),
+    ('linear_regression', 'Type inference'),
+    ('gaussian_process', 'Type inference'),
+    ('regression_matrix', 'Type inference'),
+    ('squared_error', 'Type inference'),
+    ('vectorized_probability', 'Type inference'),
+]
+@pytest.mark.parametrize('test_name, fail', compile_tests)   
+def test_normalize_and_compare(test_name, fail):
+    if fail:
+        pytest.xfail(fail)
+    filename = f'deepppl/tests/good/{test_name}.stan'
+    target_file = f'deepppl/tests/target_py/{test_name}.py'
+    normalize_and_compare(filename, target_file)
+    
+compile_tests_notype = [
+    ('mlp', None), 
+    ('mlp_default_init', None),
+    ('kmeans', None),
+    ('schools', None),
+    ('logistic', 'Type infernce'), 
+    ('lda', None),
+    ('paper/bayes_nn', None),
+    ('paper/coin_guide', None),
+    ('paper/seeds', None),
+    ('paper/coin_vect', None),
+    ('paper/coin', None),
+    ('paper/multimodal', None),
+    ('paper/multimodal_guide', None),
+    ('paper/posterior_twice', None),
+    ('paper/vae_inferred_shape', None),
+    ('paper/vae', None)  
+]
+@pytest.mark.parametrize('test_name, fail', compile_tests_notype)   
+def test_normalize_and_compare_notype(test_name, fail):
+    if fail:
+        pytest.xfail(fail)
+    filename = f'deepppl/tests/good/{test_name}.stan'
+    target_file = f'deepppl/tests/target_py/{test_name}.py'
     normalize_and_compare(filename, target_file, verbose=False)
 
+raise_tests = [
+    ('coin_guide_missing_var', MissingGuideException, None),
+    ('coin_guide_sample_obs', ObserveOnGuideException, None),
+    ('mlp_undeclared_parameters', UndeclaredParametersException, None),
+    ('mlp_undeclared_net1', UndeclaredNetworkException, None),
+    ('mlp_undeclared_net2', UndeclaredNetworkException, None),
+    ('mlp_missing_guide', MissingGuideNetException, None),
+    ('coin_unknown_identifier', UndeclaredVariableException, None),
+    ('coin_unknown_distribution', UnknownDistributionException, None),
+    ('coin_already_declared_var', AlreadyDeclaredException, None),
+    ('mlp_unsupported_prop', UnsupportedProperty, None),
+    ('mlp_unsupported_prop1', UnsupportedProperty, None),
+    ('mlp_incorrect_shape1', IncompatibleShapes, 'Type inference'),
+    ('mlp_incorrect_shape2', IncompatibleShapes, 'Type inference'),
+    ('mlp_incorrect_shape3', IncompatibleShapes, 'Type inference'),
+    ('mlp_incorrect_shape4', IncompatibleShapes, 'Type inference')
+]
 
-def test_mlp_default_init():
-    filename = r'deepppl/tests/good/mlp_default_init.stan'
-    target_file = r'deepppl/tests/target_py/mlp_init.py'
-    normalize_and_compare(filename, target_file, verbose=False)
+@pytest.mark.parametrize('test_name, error, fail', raise_tests)
+def test_raise_error(test_name, error, fail):
+    if fail:
+        pytest.xfail(fail)
+    with pytest.raises(error):
+        filename = f'deepppl/tests/good/{test_name}.stan'
+        compile(filename)
+        
+        
+not_raise_tests = [
+    ('coin_guide_missing_model', MissingModelException, None),
+    ('coin_invalid_sampling', InvalidSamplingException, None),
+    ('coin_invalid_sampling2', NonRandomSamplingException, None),
+    ('mlp_missing_model', MissingPriorNetException, 'Type inference'),
+    
+]
 
-
-def test_vae():
-    filename = r'deepppl/tests/good/vae.stan'
-    target_file = r'deepppl/tests/target_py/vae.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This test does not currently work with type inference")
-def test_linear_regression():
-    filename = r'deepppl/tests/good/linear_regression.stan'
-    target_file = r'deepppl/tests/target_py/linear_regression.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_kmeans():
-    filename = r'deepppl/tests/good/kmeans.stan'
-    target_file = r'deepppl/tests/target_py/kmeans.py'
-    normalize_and_compare(filename, target_file, verbose=False)
-
-
-def test_schools():
-    filename = r'deepppl/tests/good/schools.stan'
-    target_file = r'deepppl/tests/target_py/schools.py'
-    normalize_and_compare(filename, target_file, verbose=False)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_gaussian_process():
-    filename = r'deepppl/tests/good/gaussian_process.stan'
-    target_file = r'deepppl/tests/target_py/gaussian_process.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_missing_data():
-    filename = r'deepppl/tests/good/missing_data.stan'
-    target_file = r'deepppl/tests/target_py/missing_data.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_regression_matrix():
-    filename = r'deepppl/tests/good/regression_matrix.stan'
-    target_file = r'deepppl/tests/target_py/regression_matrix.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_logistic():
-    filename = r'deepppl/tests/good/logistic.stan'
-    target_file = r'deepppl/tests/target_py/logistic.py'
-    normalize_and_compare(filename, target_file)
-
-# def test_row_vector_expr_terms():
-#     filename = r'deepppl/tests/good/row_vector_expr_terms.stan'
-#     target_file = r'deepppl/tests/target_py/row_vector_expr_terms.py'
-#     normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_gradient_warn():
-    filename = r'deepppl/tests/good/gradient_warn.stan'
-    target_file = r'deepppl/tests/target_py/gradient_warn.py'
-    normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_squared_error():
-    filename = r'deepppl/tests/good/squared_error.stan'
-    target_file = r'deepppl/tests/target_py/squared_error.py'
-    normalize_and_compare(filename, target_file)
-
-# def test_validate_arr_expr_primitives():
-#     filename = r'deepppl/tests/good/validate_arr_expr_primitives.stan'
-#     target_file = r'deepppl/tests/target_py/validate_arr_expr_primitives.py'
-#     normalize_and_compare(filename, target_file)
-
-
-@pytest.mark.xfail(strict=False, reason="This currently fails with type inference.  Reasons not yet investigated.")
-def test_vectorized_probability():
-    filename = r'deepppl/tests/good/vectorized_probability.stan'
-    target_file = r'deepppl/tests/target_py/vectorized_probability.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_lda():
-    filename = r'deepppl/tests/good/lda.stan'
-    target_file = r'deepppl/tests/target_py/lda.py'
-    normalize_and_compare(filename, target_file, verbose=False)
-
-
-def test_neal_funnel():
-    filename = r'deepppl/tests/good/neal_funnel.stan'
-    target_file = r'deepppl/tests/target_py/neal_funnel.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_cockroaches():
-    filename = r'deepppl/tests/good/cockroaches.stan'
-    target_file = r'deepppl/tests/target_py/cockroaches.py'
-    normalize_and_compare(filename, target_file)
-
-
-def test_seeds():
-    filename = r'deepppl/tests/good/seeds.stan'
-    target_file = r'deepppl/tests/target_py/seeds.py'
-    normalize_and_compare(filename, target_file)
+@pytest.mark.parametrize('test_name, error, fail', not_raise_tests)
+def test_not_raise_error(test_name, error, fail):
+    if fail:
+        pytest.xfail(fail)
+    with not_raises(error):
+        filename = f'deepppl/tests/good/{test_name}.stan'
+        compile(filename)
