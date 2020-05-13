24caf9aef905b766ebc2b594ec15952dad8f746d
Guillaume Baudart <guillaume.baudart@ibm.com>
2020-05-13 12:11:29 -0400

    Update cockroaches expected target

--- deepppl/tests/target_py/cockroaches.py
+++ deepppl/tests/target_py/cockroaches.py
@@ -8,29 +8,24 @@ import pyro.distributions as dist
 def transformed_data(N=None, exposure2=None, roach1=None, senior=None,
     treatment=None, y=None):
     log_expo = zeros(N)
+    sqrt_roach = zeros(N)
     log_expo = log(exposure2)
-    return {'log_expo': log_expo}
+    sqrt_roach = sqrt(roach1)
+    return {'sqrt_roach': sqrt_roach, 'log_expo': log_expo}
 
 
 def model(N=None, exposure2=None, roach1=None, senior=None, treatment=None,
     y=None, transformed_data=None):
+    sqrt_roach = transformed_data['sqrt_roach']
     log_expo = transformed_data['log_expo']
     beta = sample('beta', ImproperUniform(shape=4))
-    lambda_ = sample('lambda_', ImproperUniform(shape=N))
-    tau = sample('tau', LowerConstrainedImproperUniform(0.0))
-    sigma = 1.0 / sqrt(tau)
-    sample('tau' + '__1', dist.Gamma(0.001, 0.001), obs=tau)
-    sample('lambda_' + '__2', dist.Normal(zeros(N), sigma), obs=lambda_)
-    sample('y' + '__3', poisson_log(lambda_ + log_expo + beta[1 - 1] + beta
-        [2 - 1] * roach1 + beta[3 - 1] * senior + beta[4 - 1] * treatment),
-        obs=y)
-
-
-def generated_quantities(N=None, exposure2=None, roach1=None, senior=None,
-    treatment=None, y=None, transformed_data=None, parameters=None):
-    log_expo = transformed_data['log_expo']
-    beta = parameters['beta']
-    lambda_ = parameters['lambda_']
-    tau = parameters['tau']
-    sigma = 1.0 / sqrt(tau)
-    return {'sigma': sigma}
+    sample('beta' + '__{}'.format(1 - 1) + '__1', dist.Normal(0, 5), obs=
+        beta[1 - 1])
+    sample('beta' + '__{}'.format(2 - 1) + '__2', dist.Normal(0, 2.5), obs=
+        beta[2 - 1])
+    sample('beta' + '__{}'.format(3 - 1) + '__3', dist.Normal(0, 2.5), obs=
+        beta[3 - 1])
+    sample('beta' + '__{}'.format(4 - 1) + '__4', dist.Normal(0, 2.5), obs=
+        beta[4 - 1])
+    sample('y' + '__5', poisson_log(log_expo + beta[1 - 1] + beta[2 - 1] *
+        sqrt_roach + beta[3 - 1] * treatment + beta[4 - 1] * senior), obs=y)
